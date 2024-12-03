86ca5aeb30c5863f340f2db446ff8b4c94b4e3c0
Jay <jay@gmail.com>
2024-12-03 12:09:35 -0500

    day 2 session 1

--- mysite/Auth_app/admin.py
+++ mysite/Auth_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Contact)
+
+# @admin.register(models.Contact)
+# class ContactAdmin(admin.ModelAdmin):
+#     list_display = ("id",'fullname', 'email', 'phone', 'message')
+#     search_fields = ('fullname', 'email', 'phone', 'message')
+#     list_filter = ('fullname', 'email', 'phone', 'message')
--- mysite/Auth_app/models.py
+++ mysite/Auth_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fullname=models.CharField(max_length=256)
+    email=models.EmailField()
+    phone=models.CharField(max_length=10)
+    message=models.TextField()
