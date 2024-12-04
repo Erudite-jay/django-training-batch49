a32267aa3288619e9683b59c90581c130b62baae
Jay <jay@gmail.com>
2024-12-04 11:53:50 -0500

    Day 3 session 1

--- mysite/Auth_app/urls.py
+++ mysite/Auth_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('print/',views.print_hello),
     path('home/',views.home_page),
     path("data/",views.get_data),
+    path("save-data/",views.save_data),
+    path("sud/<int:pk>/",views.single_user_data),
 ]
--- mysite/Auth_app/views.py
+++ mysite/Auth_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 from .models import Contact
 from .serializers import ContactSerializer
+
+import json
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def print_hello(request):
@@ -19,5 +22,72 @@ def get_data(request):
 
             return JsonResponse(serialized_data.data,safe=False,status=200) 
         
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+@csrf_exempt    
+def save_data(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            serialized_data = ContactSerializer(data=data)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return JsonResponse({"message": "Data saved successfully"}, status=200)
+            else:
+                return JsonResponse({"error": serialized_data.errors}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+@csrf_exempt
+def single_user_data(request,pk):
+    if request.method == 'GET':
+        try:
+            user = Contact.objects.get(id=pk) #object #queryset
+
+            serialized_data = ContactSerializer(user) #serialized data
+            return JsonResponse(serialized_data.data, status=200)
+        except Contact.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+    
+    if request.method=="PUT":
+        try:
+            user = Contact.objects.get(id=pk) #finding the user 
+            input_data = json.loads(request.body) 
+            serialized_data = ContactSerializer(user, data=input_data)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return JsonResponse({"message": "Data updated successfully"}, status=200)
+            else:
+                return JsonResponse({"error": serialized_data.errors}, status=400)
+        except Contact.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+        
+    if request.method == 'PATCH':
+        try:
+            user = Contact.objects.get(id=pk) #finding the user 
+            input_data = json.loads(request.body) 
+            serialized_data=ContactSerializer(user,data=input_data,partial=True)
+
+            if serialized_data.is_valid():
+                serialized_data.save()
+            return JsonResponse({"message": "Data updated successfully"}, status=200)
+        
+        except Contact.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+    if request.method == 'DELETE':
+        try:
+            user = Contact.objects.get(id=pk) #finding the user 
+            user.delete()
+            return JsonResponse({"message": "Data deleted successfully"}, status=204)
+        except Contact.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
         except Exception as e:
             return JsonResponse({"error": str(e)})
